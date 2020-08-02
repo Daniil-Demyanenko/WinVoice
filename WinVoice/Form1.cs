7c2b8b8b6e2c9765045a3ec970dd46219d7250b0
Daniil-Demyanenko <daniil-demyanenko1@mail.ru>
2020-08-02 15:56:47 +0300

    Добавьте файлы проекта.

---
+++ WinVoice/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinVoice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.Manual;
+            Point pt = Screen.PrimaryScreen.WorkingArea.Location;
+            pt.Offset(Screen.PrimaryScreen.WorkingArea.Width, Screen.PrimaryScreen.WorkingArea.Height);
+            pt.Offset(-this.Width, -this.Height);
+            this.Location = pt;
+
+            //this.Hide();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void setWindowPosition()
+        {
+            //настройка размеров pictureBox
+            pictureBox1.Width = pictureBox1.Image.Width;
+            pictureBox1.Height = pictureBox1.Image.Height;
+            //настройка размеров окна равных pictureBox
+            this.Height = pictureBox1.Height;
+            this.Width = pictureBox1.Width;
+            //установка положения окна внизу справа
+            Point pt = Screen.PrimaryScreen.WorkingArea.Location;
+            pt.Offset(Screen.PrimaryScreen.WorkingArea.Width, Screen.PrimaryScreen.WorkingArea.Height);
+            pt.Offset(-this.Width, -this.Height);
+            this.Location = pt;
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            if (m.Msg == 0x219)
+            {
+                if (m.WParam.ToInt32() == 0x8000)
+                {
+                    //новое usb подключено
+                    MessageBox.Show("New dev connected");
+                }
+                if (m.WParam.ToInt32() == 0x8004)
+                {
+                    // usb отключено
+                    MessageBox.Show("Dev disconnected");
+                }
+
+            }
+        }
+    }
+}
